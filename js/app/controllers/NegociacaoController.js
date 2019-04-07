beb05d6b12aaafb4e4cb5498ff2738a1d3a3c493
Diego Delmiro <diegoalbuquerque03@gmail.com>
2019-04-07 13:19:44 -0300

    Realizando o parse da resposta.

--- js/app/controllers/NegociacaoController.js
+++ js/app/controllers/NegociacaoController.js
@@ -60,9 +60,12 @@ class NegociacaoController {
             if(xhr.readyState == 4){
                 
                 if(xhr.status == 200){
-                    console.log('Obtendo as negociações do servidor.');    
+                   JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)).
+                   forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                   this._mensagem.texto = 'Negociações importadas com sucesso.'; 
                 }else{
-                    console.log('ERRO NO SERVIDOR.');
+                    console.log(xhr.responseText);
+                    this._mensagem.texto = "Erro ao obter as negociações da semana.";
                 }
             }
         }
